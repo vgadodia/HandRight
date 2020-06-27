67b523b74eff992dc96dc21cb4f13dabd40a4f96
Veer Gadodia <veergadodia24@gmail.com>
2020-06-27 00:15:03 -0400

    camera

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 from importlib import reload
 from flask import Flask, render_template, redirect, request, url_for, Response
 from camera import VideoCamera
+import camera
 
 # Needed for encoding to utf8
 reload(sys)
@@ -118,10 +119,17 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame[0] + b'\r\n\r\n')
+        # if frame[1] == False:
+        #     yield (b'--frame\r\n'
+        #         b'Content-Type: image/jpeg\r\n\r\n' + frame[0] + b'\r\n\r\n')
+        # else:
+        #     return (b'--frame\r\n'
+        #         b'Content-Type: image/jpeg\r\n\r\n' + frame[0] + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
+    # print(gen(VideoCamera()))
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
--- camera.py
+++ camera.py
@@ -2,20 +2,158 @@ import cv2
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 ds_factor=0.6
 
+from imutils.video import VideoStream
+from imutils.video import FPS
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import matplotlib.pyplot as plt
+import time
+
+def decode_predictions(scores, geometry):
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the
+		# geometrical data used to derive potential bounding box
+		# coordinates that surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability,
+			# ignore it
+			if scoresData[x] < 0.5:
+				continue
+
+			# compute the offset factor as our resulting feature
+			# maps will be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+			# extract the rotation angle for the prediction and
+			# then compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+
+			# use the geometry volume to derive the width and height
+			# of the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+
+			# compute both the starting and ending (x, y)-coordinates
+			# for the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+
+			# add the bounding box coordinates and probability score
+			# to our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+
+	# return a tuple of the bounding boxes and associated confidences
+	return (rects, confidences)
+
+(W, H) = (None, None)
+(newW, newH) = (320, 320)
+(rW, rH) = (None, None)
+
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+
+net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+
+vs = cv2.VideoCapture(0)
+i = 0
+
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
+        self.W = None
+        self.H = None
+        self.i = 0
     
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
-        success, image = self.video.read()
-        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
-        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
-        for (x,y,w,h) in face_rects:
-        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-        	break
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        frame = self.video.read()
+        frame = frame[1]
+        isFinished = False
+
+        # resize the frame, maintaining the aspect ratio
+        orig = frame.copy()
+
+        # if our frame dimensions are None, we still need to compute the
+        # ratio of old frame dimensions to new frame dimensions
+        if self.W is None or self.H is None:
+            (H, W) = frame.shape[:2]
+            rW = W / float(newW)
+            rH = H / float(newH)
+
+        # resize the frame, this time ignoring aspect ratio
+
+        # construct a blob from the frame and then perform a forward pass
+        # of the model to obtain the two output layer sets
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (newW, newH),
+            (123.68, 116.78, 103.94), swapRB=True, crop=False)
+        net.setInput(blob)
+        (scores, geometry) = net.forward(layerNames)
+
+        # decode the predictions, then  apply non-maxima suppression to
+        # suppress weak, overlapping bounding boxes
+        (rects, confidences) = decode_predictions(scores, geometry)
+        boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+        # loop over the bounding boxes
+        for (startX, startY, endX, endY) in boxes:
+            self.i += 1
+            # scale the bounding box coordinates based on the respective
+            # ratios
+            startX = int(startX * rW)
+            startY = int(startY * rH)
+            endX = int(endX * rW)
+            endY = int(endY * rH)
+
+            # draw the bounding box on the frame
+            cv2.rectangle(frame, (startX-30, startY-50), (endX+70, endY+20), (0, 255, 0), 2)
+
+            cropped = orig[startY-50:endY+20, startX-30:endX+70]
+            # print(len(cropped))
+            # plt.imshow(cropped)
+            if self.i == 10:
+                cv2.imwrite('text.png', cropped)
+                isFinished = True
+
+        # show the output frame
+        # cv2.imshow("Text Detection", orig)
+        # key = cv2.waitKey(1) & 0xFF
+
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return [jpeg.tobytes(), isFinished]
+
+        # success, image = self.video.read()
+        # image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        # gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        # face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        # for (x,y,w,h) in face_rects:
+        # 	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        # 	break
